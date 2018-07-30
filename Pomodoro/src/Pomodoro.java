9da7e9af47c22885cdbcec7edf4ccce01697d20f
BlueMan2903 <40451693+BlueMan2903@users.noreply.github.com>
2018-07-30 11:45:25 +0300

    Update Pomodoro.java
    added a wav alarm beep

--- Pomodoro/src/Pomodoro.java
+++ Pomodoro/src/Pomodoro.java
@@ -2,6 +2,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
+import java.io.File;
+import javax.sound.sampled.*;
 
 public class Pomodoro {
 	
@@ -25,27 +27,53 @@ class Task implements Runnable{
 			es.schedule(() -> System.out.println("Time to take a break!\nPress enter: "), 
 					25, 
 					TimeUnit.MINUTES);
+			play("alarm.wav");
 			scanner.nextLine();
 			es.schedule(() -> System.out.println("Go back to work, peon!\nPress enter: "),
 					5,
 					TimeUnit.MINUTES);
+			play("alarm.wav");
 			scanner.nextLine();
 		}
 		
-		es.schedule(() -> System.out.println("Time to take a break!\nPress enter: "),
+		es.schedule(() -> System.out.println("Time for a BIG BREAK!!!\nPress enter: "),
 					25,
 					TimeUnit.MINUTES);
+		play("alarm.wav");
 		scanner.hasNextLine();
 		es.schedule(() -> System.out.println("Go back to work, peon!\nPress enter: "),
 					30,
 					TimeUnit.MINUTES);
+		play("alarm.wav");
 		scanner.nextLine();
 	}
 	
 	public static void main(String[] args) {
 		(new Thread(new Task())).start();	
 	}
+	
+	public static void play(String fileName) {
+		
+		try {
+			File soundFile = new File(fileName);
+			AudioInputStream soundIn = AudioSystem.getAudioInputStream(soundFile);
+			AudioFormat format = soundIn.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			Clip clip = (Clip)AudioSystem.getLine(info);
+			clip.open(soundIn);
+			clip.start();
+			while(clip.isRunning()) {
+				Thread.yield();
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace(System.out);
+		}
+	}
 }
+
+
+
 
 
 
